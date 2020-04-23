f90009d3f00bb0e36dde23bc6f02963d56044137
wangzuzhen <380429867@qq.com>
2020-04-23 18:03:06 +0800

    CAS Demo, 单例模式

---
+++ juc/src/com/atguigu/sh/juc/ms/AtomicReferenceDemo.java
@@ -0,0 +1,19 @@
+package com.atguigu.sh.juc.ms;
+
+/**
+ * @version 1.0
+ * @ClassName: AtomicReferenceDemo
+ * @Description: 原子引用
+ * @Author zzwang<br />
+ * @Date: 2020/4/23 16:48
+ */
+
+
+class User {
+    private Integer age;
+    private String userName;
+}
+
+public class AtomicReferenceDemo {
+
+}
---
+++ juc/src/com/atguigu/sh/juc/ms/SingletonDemo.java
@@ -0,0 +1,43 @@
+package com.atguigu.sh.juc.ms;
+
+/**
+ * 单例模式
+ */
+public class SingletonDemo {
+
+    private static volatile SingletonDemo instance = null;
+
+    private SingletonDemo() {
+        System.out.println(Thread.currentThread().getName()  + "\t 构造方法..." );
+    }
+
+    // 加synchronized
+    // DCL ： 双端检索机制
+    public static SingletonDemo getInstance() {
+
+        if(instance == null) {
+            synchronized (SingletonDemo.class) {
+                if(instance == null) {
+                    instance = new SingletonDemo();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+
+        /*System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());*/
+
+        for (int i = 1; i <= 50; i++) {
+        	new Thread(() -> {
+                SingletonDemo.getInstance();
+        	}, String.valueOf(i)).start();
+        }
+
+
+    }
+
+}
